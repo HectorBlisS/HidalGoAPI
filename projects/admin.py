1482fe73239a6d06d43ac37edfbcbffce89307c5
BlisS <contacto@fixter.org>
2016-10-22 17:35:14 -0500

    files

--- projects/admin.py
+++ projects/admin.py
@@ -4,6 +4,7 @@ from .models import Project,Comment,Category
 
 class ProjectAdmin(admin.ModelAdmin):
 	prepopulated_fields = {'slug':('title',)}
+	list_display = ['id','title']
 
 admin.site.register(Project, ProjectAdmin)
 admin.site.register(Comment)
--- projects/views.py
+++ projects/views.py
@@ -79,7 +79,7 @@ class ProjectListView(View):
 	def post(self,request):
 		try:
 
-			form = ProjectForm(request.POST)
+			form = ProjectForm(request.POST,request.FILES)
 			if form.is_valid():
 				form.save()
 
@@ -99,9 +99,9 @@ class ProjectListView(View):
 
 
 class ProjectDetailView(View):
-	@method_decorator(csrf_exempt)
-	def dispatch(self, request, *args, **kwargs):
-		return super(ProjectDetailView, self).dispatch(request, *args, **kwargs)
+	# @method_decorator(csrf_exempt)
+	# def dispatch(self, request, *args, **kwargs):
+	# 	return super(ProjectDetailView, self).dispatch(request, *args, **kwargs)
 
 	def get(self,request,id):
 		# id=request.GET.get('id')
@@ -127,9 +127,15 @@ class ProjectDetailView(View):
 		return HttpResponse(data,content_type = 'application/javascript; charset=utf8')
 
 	def post(self,request,id):
+		project = get_object_or_404(Project,id=id)
+		form = ProjectForm(data=request.POST,files=request.FILES,instance=project)
+		o = form.save(commit=False)
+		print(o)
+		print(request.POST)
+		print(request.FILES)
+		print(request.POST.get('img'))
 		try:
-			project = get_object_or_404(Project,id=id)
-			form = ProjectForm(request.POST,request.FILES,instance=project)
+			
 
 			if form.is_valid():
 				form.save()
