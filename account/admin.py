a68048e03852886bd5481df2c81e87f25da76314
BlisS <contacto@fixter.org>
2016-11-02 14:14:05 -0600

    export profiles

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Profile
 
+from import_export.admin import ImportExportMixin, ImportMixin, ExportActionModelAdmin, ImportExportActionModelAdmin
 
-admin.site.register(Profile)
+
+class ProfileAdmin(ImportExportActionModelAdmin, ImportExportMixin, admin.ModelAdmin):
+	# prepopulated_fields = {'slug':('title',)}
+	list_display = ['id','name','edad','email']
+	list_filter = ['edad']
+	search_fields = ['id','name','email']
+
+
+admin.site.register(Profile, ProfileAdmin)
